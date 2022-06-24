6e17bb63f69bfc72ce520c33dde563852936dfe1
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-06-24 23:48:43 +0530

    set websiti

--- src/main/java/lk/ijse/spring/config/WebAppConfig.java
+++ src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -4,15 +4,33 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "lk.ijse.spring")
-public class WebAppConfig {
+public class WebAppConfig implements WebMvcConfigurer {
 
     @Bean
     public ModelMapper modelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver v= new InternalResourceViewResolver();
+        v.setPrefix("/");
+        v.setSuffix(".html");
+        v.setOrder(2);
+        return v;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/");
     }
 }
